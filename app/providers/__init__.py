f7023eca5c98fd272b603366b40fe4798fe1af00
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-26 22:08:18 +0200

    Use strict Wikimedia provider in v0.3.6

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,5 +1,5 @@
 from .tmdb import TmdbProvider
 from .tvmaze import TvmazeProvider
-from .wikimedia import WikimediaProvider
+from .wikimedia_v036 import WikimediaProvider
 
 __all__ = ["TmdbProvider", "TvmazeProvider", "WikimediaProvider"]
