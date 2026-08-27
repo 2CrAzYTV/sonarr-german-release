e97bc3bfe17465482e6d09f617cc3a147659be13
2CrAzYTV <104258629+2CrAzYTV@users.noreply.github.com>
2026-08-27 06:32:18 +0200

    Export fernsehserien.de provider

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,5 +1,11 @@
+from .fernsehserien import FernsehserienProvider
 from .tmdb import TmdbProvider
 from .tvmaze import TvmazeProvider
 from .wikimedia_v037 import WikimediaProvider
 
-__all__ = ["TmdbProvider", "TvmazeProvider", "WikimediaProvider"]
+__all__ = [
+    "FernsehserienProvider",
+    "TmdbProvider",
+    "TvmazeProvider",
+    "WikimediaProvider",
+]
